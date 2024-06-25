0142452371039416565bc48c5f60bc3f45933f85
ohanz <chiaguguadaniel@gmail.com>
2024-06-25 02:42:22 +0100

    new tempo commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function App() {
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
+import {View, Text, Button} from 'react-native';
+
+type DetailsScreenParams = {id: string};
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: DetailsScreenParams;
+};
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function HomeScreen() {
+  const navigation = useNavigation();
   return (
-    <View style={styles.container}>
-      <Text>This is a todo APP!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details', {id: '1234'})}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+type DetailsScreenProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
+
+function DetailsScreen({route}: DetailsScreenProps) {
+  const {id} = route.params;
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+      <Text>{id}</Text>
+    </View>
+  );
+}
+
+function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Overview'}}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
